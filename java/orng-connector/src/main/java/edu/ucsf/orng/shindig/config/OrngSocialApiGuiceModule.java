33f26e3e31ee97cd09166aabccef14d089eaedb1
Eric Meeks <eric.meeks@ucsf.edu>
2013-09-25 15:16:46 -0700

    Removing unnecessary registry service it is no longer needed with the rdf integration

--- java/orng-connector/src/main/java/edu/ucsf/orng/shindig/config/OrngSocialApiGuiceModule.java
+++ java/orng-connector/src/main/java/edu/ucsf/orng/shindig/config/OrngSocialApiGuiceModule.java
@@ -8,7 +8,6 @@ import org.apache.shindig.social.opensocial.service.AppDataHandler;
 import org.apache.shindig.social.opensocial.service.MessageHandler;
 import org.apache.shindig.social.opensocial.service.PersonHandler;
 import edu.ucsf.orng.shindig.service.RdfHandler;
-import edu.ucsf.orng.shindig.service.RegistryHandler;
 
 import com.google.common.collect.ImmutableSet;
 
@@ -20,6 +19,6 @@ public class OrngSocialApiGuiceModule extends SocialApiGuiceModule {
 	   */
 	  protected Set<Class<?>> getHandlers() {
 	    return ImmutableSet.<Class<?>>of(ActivityHandler.class, AppDataHandler.class,
-	        PersonHandler.class, MessageHandler.class, RdfHandler.class, RegistryHandler.class);
+	        PersonHandler.class, MessageHandler.class, RdfHandler.class);
 	  }
 }
