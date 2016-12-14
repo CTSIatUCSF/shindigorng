769515151d9b61be06b1d88121411e190a3960d6
Eric Meeks <eric.meeks@ucsf.edu>
2016-12-14 12:55:32 -0800

    Added Fueski back and updated HttpClient

--- src/main/java/edu/ucsf/orng/shindig/spi/rdf/RdfService.java
+++ src/main/java/edu/ucsf/orng/shindig/spi/rdf/RdfService.java
@@ -5,7 +5,13 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,20 +22,28 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.shindig.auth.SecurityToken;
 import org.apache.shindig.common.cache.Cache;
 import org.apache.shindig.common.cache.CacheProvider;
+import org.apache.shindig.common.servlet.GuiceServletContextListener.CleanupCapable;
+import org.apache.shindig.common.servlet.GuiceServletContextListener.CleanupHandler;
 import org.apache.shindig.gadgets.http.HttpFetcher;
 import org.apache.shindig.protocol.ProtocolException;
 import org.apache.shindig.social.opensocial.spi.UserId;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
+import edu.ucsf.ctsi.r2r.jena.DbService;
+import edu.ucsf.ctsi.r2r.jena.FusekiCache;
 import edu.ucsf.ctsi.r2r.jena.LODService;
+import edu.ucsf.ctsi.r2r.jena.ResourceService;
+import edu.ucsf.ctsi.r2r.jena.SparqlQueryClient;
 import edu.ucsf.orng.shindig.config.OrngProperties;
 import edu.ucsf.orng.shindig.spi.OrngDBUtil;
 
 @Singleton
-public class RdfService implements OrngProperties {
+public class RdfService implements OrngProperties, CleanupCapable {
 
 	private static final Logger LOG = Logger.getLogger(RdfService.class.getName());
 	
@@ -43,9 +57,16 @@ public class RdfService implements OrngProperties {
 	private Cache<String, Model> modelCache;
 	private Cache<String, String> appInstanceCache;
 	
+	private FusekiCache userCache;
+	private FusekiCache anonymousCache;
+	private ScheduledExecutorService executorService;
+	
 	@Inject
 	public RdfService(@Named("orng.system") String system, @Named("orng.systemDomain") String systemDomain, 
-							  OrngDBUtil dbUtil, CacheProvider cacheProvider, HttpFetcher fetcher) throws Exception {
+							  @Named("orng.fuseki") String fuseki, @Named("orng.fusekiDBUser") String orngUser, 
+							  @Named("orng.fusekiFetchIntervalMinutes") String fetchInterval, @Named("orng.fusekiEagerRunLimitMinutes") String eagerRunLimit,
+							  OrngDBUtil dbUtil, CacheProvider cacheProvider, HttpFetcher fetcher,
+							  CleanupHandler cleanup) throws Exception {
 		this.system = system;
 		this.systemDomain = systemDomain;
 		this.systemBase = systemDomain;
@@ -63,6 +84,30 @@ public class RdfService implements OrngProperties {
     		modelCache = cacheProvider.createCache("orngRdf");
     		appInstanceCache = cacheProvider.createCache("orgnAppInstance");
     	}
+    	if (cleanup != null) {
+    		cleanup.register(this);
+    	}
+
+    	if (StringUtils.isNotBlank(fuseki)) {
+    		String fusekiURL = fuseki;
+	    	userCache = new FusekiCache(new SparqlQueryClient(fusekiURL + "/query"),
+	    								new ShindigSparqlPostClient(fusekiURL + "/update", fusekiURL + "/data?default", fetcher), 
+	    								new DbService(systemDomain, orngUser, dbUtil));
+	    	//userService = new TDBCacheResourceService(system, systemDomain, tdbBaseDir + orngUser, tdbCacheExpire, new DbModelService(systemDomain, orngUser, dbUtil));
+	    	//anonymousService = new TDBCacheResourceService(system, systemDomain, tdbBaseDir + ANONYMOUS, tdbCacheExpire, new DbModelService(systemDomain, null, dbUtil));
+	    	
+	    	// pass into some scheduled loader
+	    	executorService = Executors.newScheduledThreadPool(1, new ThreadFactory() {
+	    		   public Thread newThread(Runnable runnable) {
+	    		      Thread thread = Executors.defaultThreadFactory().newThread(runnable);
+	    		      thread.setDaemon(true);
+	    		      return thread;
+	    		   }
+	    		});
+	    	// start with a delay equal to the run limit.  Shindig is busy at startup, so best to wait 
+	    	executorService.scheduleAtFixedRate(new EagerFetcher(userCache, Integer.parseInt(eagerRunLimit)), 
+	    			Integer.parseInt(eagerRunLimit), Integer.parseInt(fetchInterval), TimeUnit.MINUTES);
+	    	}
 	}
 	
 	private Model getFromModelCache(String key) {
@@ -98,11 +143,13 @@ public class RdfService implements OrngProperties {
 	// maybe have some option to force fresh or allow cache, etc.
 	public RdfItem getRDF(String url, boolean nocache, boolean expand, Set<String> fields, String sessionId, SecurityToken token) throws Exception {		
 		String uri = getURI(url);
+		boolean local = false;
 		String viewerId =  token.getViewerId();
 		boolean anonymous = viewerId == null || "-1".equals(viewerId);
 		
 		// custom way to convert URI to URL in case standard LOD mechanisms will not work
 		if (systemDomain != null && url.toLowerCase().startsWith(systemDomain.toLowerCase())) {
+			local = true;
 			// not sure where this logic belongs, but this seems as good as any
 			if (SYS_PROFILES.equalsIgnoreCase(system)) {
 				expand |= url.contains("ShowDetails=true");
@@ -111,6 +158,17 @@ public class RdfService implements OrngProperties {
 		
 		String cacheKey = uri + anonymous + expand;
 		Model model = getFromModelCache(cacheKey);
+		if (model != null) {
+			return new RdfItem(model, uri);
+		}
+		// we get non URI's coming into here, need to find a better way to work with those.
+		if (local && uri.indexOf('?') == -1 && !nocache && !expand) {
+			FusekiCache cache = anonymous ? anonymousCache : userCache;
+			if (cache != null) {
+				model = (fields == null || fields.size() == 0) ? cache.getModel(uri) : cache.getModel(uri, fields);
+			}
+		}
+		
 		if (model == null) {
 			// this can grab anything, and knows how to directly grab data from a local Profiles
 			LODService service = new LODService(systemDomain, sessionId, viewerId, true, expand);
@@ -257,5 +315,70 @@ public class RdfService implements OrngProperties {
 			}
 		}
 	}
-				
+			
+	// important to put this in only 1 thread!
+	private class EagerFetcher implements Runnable {
+
+		private ResourceService service;
+		private List<String> currentURIs;
+		private int runTimeInMinutes; // how long to run before giving the machine a needed break....
+		
+		private EagerFetcher(ResourceService service, int runTimeInMinutes) {
+			this.service = service;
+			this.runTimeInMinutes = runTimeInMinutes;
+			this.currentURIs = new ArrayList<String>();
+		}
+		
+		private synchronized void loadProfileURIs() throws SQLException {
+			LOG.info("Loading ProfileURI's to fetch");
+	        Connection conn = dbUtil.getConnection();        
+			try {
+		        ResultSet rs = conn.createStatement().executeQuery("SELECT nodeId FROM [ORNG.].[vwPerson] WHERE isActive = 1");        
+		        while (rs.next()) {
+		        	currentURIs.add(systemBase + rs.getInt(1));
+		        	LOG.log(Level.FINE, "Adding " + rs.getInt(1) + " to eager load list");
+	        	}
+		        rs.close();	        
+			}
+			finally {
+				conn.close();
+			}
+		}
+		
+		public synchronized void run() {
+			List<String> processedURIs = new ArrayList<String>();
+			try {
+				if (currentURIs.isEmpty()) {
+					loadProfileURIs();
+				}
+				// load these things!
+				DateTime start = new DateTime();
+				for (String uri : currentURIs) {
+					processedURIs.add(uri);
+					service.getResource(uri);
+					if (new Period(start, new DateTime()).getMinutes() > runTimeInMinutes) {
+						LOG.info("Halting, out of time");
+						break;
+					}
+				}
+			}
+			catch (Exception e) {
+				LOG.log(Level.WARNING, e.getMessage(), e);
+			}
+			finally {
+				// we at least TRIED to get these....
+				LOG.info("Processed " + processedURIs.size() + " URI's");
+				currentURIs.removeAll(processedURIs);				
+			}
+		}		
+	}
+	
+	public void cleanup() {
+		// since this is running in a daemon thread, this really isn't necessary
+		if (executorService != null) {
+			executorService.shutdown();
+		}
+	}
+	
+
 }
