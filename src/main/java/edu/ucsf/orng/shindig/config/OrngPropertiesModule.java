6b1925d66d8bd575f6c2df0deb4deeb062e193bf
Eric Meeks <eric.meeks@ucsf.edu>
2017-11-08 09:53:53 -0800

    No longer setting hostname dynamically, better to let environment figure itself out. Needed for a multi-hosting environment

--- src/main/java/edu/ucsf/orng/shindig/config/OrngPropertiesModule.java
+++ src/main/java/edu/ucsf/orng/shindig/config/OrngPropertiesModule.java
@@ -1,29 +1,18 @@
 package edu.ucsf.orng.shindig.config;
 
-import java.net.MalformedURLException;
-
 import org.apache.shindig.common.PropertiesModule;
 
 public class OrngPropertiesModule extends PropertiesModule implements OrngProperties {
 	
 	public OrngPropertiesModule() {
 		super(System.getProperty("shindig.contextroot").length() > 1 ? System.getProperty("shindig.contextroot").substring(1) + ".properties" : "shindigorng.properties");
-		java.net.URL hostName;
-		try {
-			hostName = new java.net.URL(getProperties().getProperty("orng.systemDomain"));
-		} catch (MalformedURLException e1) {
-			throw new RuntimeException(e1);
-		}
-		System.setProperty("shindig.host", hostName.getHost());
 	}
 	
 	@Override
 	protected void configure() {
 		super.configure();
-		// hope this is soon enough
-		// This allows us to host shindigorng on a server with multiple host names (eg. profiles.ucsf.edu, profiles.usc.edu) 
-		
-	    try {
+
+		try {
 	    	Class.forName(getProperties().getProperty("orng.dbDriver"));
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
